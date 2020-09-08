8b534e4ed5fc4272f139e940aa0c826942394d5b
Mario <mavega@uvm.edu>
2020-09-07 20:23:30 -0400

    adding height and width of browser window in leapInfiniteLoop.js, del 47

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,5 +1,10 @@
 var controllerOptions = {};
 var i = 0;
+//set window width 1/2 as x 
+var x = window.innerWidth*1/2 ;
+//set window height 1/2 as y
+var y = window.innerHeight*1/2;
+
 Leap.loop(controllerOptions, function(frame){
 	console.log(i);
 	i++;
