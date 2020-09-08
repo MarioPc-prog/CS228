d534910976213ffadee049aa43fb9b09477e8d2d
Mario <mavega@uvm.edu>
2020-09-07 20:52:39 -0400

    Modified the leapInfiniteLoop.js file and added the clear of trailing code , del 50

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,12 +1,10 @@
 var controllerOptions = {};
-var i = 0;
 //set window width 1/2 as x 
 var x = window.innerWidth*1/2 ;
 //set window height 1/2 as y
 var y = window.innerHeight*1/2;
 Leap.loop(controllerOptions, function(frame){
-	console.log(i);
-	i++;
+	clear();
 	circle(x,y,50);
 	var random = Math.random();
 	x = x + random;
