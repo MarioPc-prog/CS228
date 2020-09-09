7e7c7c316c91199c64b7c9ade99c8e9e2577ab61
Mario <mavega@uvm.edu>
2020-09-08 22:27:29 -0400

    added frame.hand , del 54

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,13 +4,16 @@ var x = window.innerWidth*1/2 ;
 //set window height 1/2 as y
 var y = window.innerHeight*1/2;
 Leap.loop(controllerOptions, function(frame){
-	clear();
-	circle(x,y,50);
-	var randomX = Math.random();
-	x = x + randomX;
-	var randomY = Math.random(-1);
-	y = y +randomY
-	
+	//clear();
+	//circle(x,y,50);
+	//var randomX = Math.random();
+	//x = x + randomX;
+	//var randomY = Math.random(-1);
+	//y = y +randomY
+	console.log(frame);
+	for(var h = 0; h < frame.hands.length; h++){
+    		var hand = frame.hands[h];
+	}
 
 }
 );
