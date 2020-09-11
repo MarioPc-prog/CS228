1f46a9c4abf58b36d3c91d0fe55cec54f2c905b9
Mario <mavega@uvm.edu>
2020-09-11 16:00:11 -0400

    del 61,62 complete

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,28 +1,29 @@
+
 var controllerOptions = {};
 //set window width 1/2 as x 
 var x = window.innerWidth*1/2 ;
 //set window height 1/2 as y
 var y = window.innerHeight*1/2;
-Leap.loop(controllerOptions, function(frame){
-	//clear();
-	//circle(x,y,50);
-	//var randomX = Math.random();
-	//x = x + randomX;
-	//var randomY = Math.random(-1);
-	//y = y +randomY
-	console.log(frame);
-	for(var h = 0; h < frame.hands.length; h++){
-    		var hand = frame.hands[h];
-		if (frame.hand.length==1){
-			console.log(hand);
-		}
-		var fingers=hand.fingers[h];
-	        for (let i = 0;i>fingers.length;i++){
-			console.log(fingers[i]);
-		}
 
-	}
-    }
+function HandleFrame(frame){
+	 for(var h = 0; h < frame.hands.length; h++){
+                var hand = frame.hands[h];
+                if (frame.hand.length==1){
+                        console.log(hand);
+                }
+                var fingers=hand.fingers[h];
+                for (let i = 0;i>fingers.length;i++){
+                        if( fingers.type ==1){
+                        console.log(fingers[i]);
+                        }
+                }
+
+        }
+   }
+Leap.loop(controllerOptions, function(frame){
+	console.log(frame);
+	HandleFrame(frame);
+}
 );
 
 
